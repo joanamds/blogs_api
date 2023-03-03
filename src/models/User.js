a55cd97d120bd53b4a36f6fce834d291097ec85c
Joana <joanamdsantos1@gmail.com>
2023-03-03 18:35:34 -0300

    feat: model de user criada

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+const User = (sequelize, DataTypes) => {
+  const user = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  },
+    {
+      tableName: 'users',
+      timestamps: false,
+      underscored: true
+    })
+  return user;
+}
+
+module.exports = User;
