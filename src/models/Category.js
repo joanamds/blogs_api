780f6ad730de9fb70f9ecc77242793d1a9ba2624
Joana <joanamdsantos1@gmail.com>
2023-03-04 14:38:19 -0300

    feat: model Category criado

---
+++ src/models/Category.js
@@ -0,0 +1,17 @@
+const Category = (sequelize, DataTypes) => {
+  const category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    }, 
+    name: DataTypes.STRING,
+  },
+    {
+      tableName: 'categories',
+      timestamps: false
+    })
+  return category;
+}
+
+module.exports = Category;
