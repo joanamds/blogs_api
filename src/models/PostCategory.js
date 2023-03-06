335c58d12f0be8eda83948de3d99693336fbd9d6
Joana <joanamdsantos1@gmail.com>
2023-03-06 12:04:31 -0300

    feat: model PostCategory criado

---
+++ src/models/PostCategory.js
@@ -0,0 +1,35 @@
+const PostCategory = (sequelize, DataTypes) => {
+  const postcategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+  },
+  {
+    tableName: 'posts_categories',
+    timestamps: false,
+    underscored: true
+    });
+  
+  postcategory.associate = ({ BlogPost, Category }) => {
+    BlogPost.belongsToMany(Category, {
+      as: 'blog_posts',
+      through: postcategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    Category.belongsToMany(BlogPost, {
+      as: 'categories',
+      through: postcategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId'
+    })
+  }
+  return postcategory;
+}
+
+module.exports = PostCategory;
